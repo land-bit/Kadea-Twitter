7c2553f03be6a1dec3833e2e72fbf09af7031ad0
landrybit <landrybitege7@gmail.com>
2024-02-22 09:21:47 +0200

    fix(images):Affichages des images

--- src/Components/Tweets.jsx
+++ src/Components/Tweets.jsx
@@ -7,7 +7,7 @@ export default function Tweets() {
             user : {
                 name : 'CNN',
                 time : '7m',
-                src : 'src/assets/Tweet-Avatar/Profile-Photo-1.png'
+                src : '/Tweet-Avatar/Profile-Photo-1.png'
             },
             content : {
                 text : 'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.'
@@ -22,11 +22,11 @@ export default function Tweets() {
             user : {
                 name : 'The New York Times',
                 time : '2h',
-                src : 'src/assets/Tweet-Avatar/Profile-Photo6.png'
+                src : '/Tweet-Avatar/Profile-Photo6.png'
             },
             content : {
                 text : 'Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land',
-                srcImage : 'src/assets/images/image 3.png'
+                srcImage : '/images/image 3.png'
             },
             actions : {
                 comments : 19,
@@ -38,7 +38,7 @@ export default function Tweets() {
             user : {
                 name : 'Twitter',
                 time : 'Oct 29',
-                src : 'src/assets/Tweet-Avatar/Profile-Photo-2.png'
+                src : '/Tweet-Avatar/Profile-Photo-2.png'
             },
             content : {
                 text : 'BIG NEWS lol jk still Twitter',
@@ -53,7 +53,7 @@ export default function Tweets() {
             user : {
                 name : 'Twitter',
                 time : 'Oct 4',
-                src : 'src/assets/Tweet-Avatar/Profile-Photo-2.png'
+                src : '/Tweet-Avatar/Profile-Photo-2.png'
             },
             content : {
                 text : 'hello literally everyone',
@@ -68,11 +68,11 @@ export default function Tweets() {
             user : {
                 name : 'Twitter',
                 time : 'Oct 4',
-                src : 'src/assets/Tweet-Avatar/Profile-Photo-2.png'
+                src : '/Tweet-Avatar/Profile-Photo-2.png'
             },
             content : {
                 text : 'hello literally everyone',
-                srcImage : 'src/assets/images/image 3.png'
+                srcImage : '/images/image 3.png'
             },
             actions : {
                 comments : '118.7k',
--- src/Components/tweet-editor/Avatars.jsx
+++ src/Components/tweet-editor/Avatars.jsx
@@ -1,6 +1,6 @@
 export default function Avatars() {
 
     return <div className="avatar"><img 
-            src="src/assets/Avatars/Profile-Photo.png" 
+            src="/Avatars/Profile-Photo.png" 
             alt="Profil d'utilisateur" /></div>
 }
