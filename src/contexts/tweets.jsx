18cd61a3bc444c1acbc01e00d9badb106cdfd92b
Landry Bitege <lan-bit@debian.landry>
2024-04-17 10:05:11 +0200

    Essaie de json-server-vercel

--- src/contexts/tweets.jsx
+++ src/contexts/tweets.jsx
@@ -4,19 +4,25 @@ import useFetch from "../useFetch";
 export const Context = createContext(null);
 
 const TweetContext = ({ children }) => {
-  const { data: tweetData } = useFetch("http://localhost:3000/tweets");
+  const { data: tweetData } = useFetch(
+    "https://kadea-twitter.vercel.app/tweets"
+  );
   const [tweets, setTweets] = useState(null);
   useEffect(() => setTweets(tweetData), [tweetData]);
 
-  const { data: currentData } = useFetch("http://localhost:3000/current-user");
+  const { data: currentData } = useFetch(
+    "https://kadea-twitter.vercel.app/current-user"
+  );
   const [current, setCurrent] = useState(null);
   useEffect(() => setCurrent(currentData), [currentData]);
 
-  const { data: navData } = useFetch("http://localhost:3000/nav");
+  const { data: navData } = useFetch("https://kadea-twitter.vercel.app/nav");
   const [nav, setNav] = useState(null);
   useEffect(() => setNav(navData), [navData]);
 
-  const { data: trendData } = useFetch("http://localhost:3000/trends");
+  const { data: trendData } = useFetch(
+    "https://kadea-twitter.vercel.app/trends"
+  );
   const [trend, setTrend] = useState(null);
   useEffect(() => setTrend(trendData), [trendData]);
 
